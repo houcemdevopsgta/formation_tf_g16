c7a58d5b391025de87f9e2c073abf3c9472e9252
khifi houcem <khlifi@git.com>
2025-07-30 19:39:46 +0200

    modif tp1

---
+++ tp1/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+   region = "eu-central-1"
+   access_key = "PUT YOUR OWN"
+   secret_key = "PUT YOUR OWN"
+
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-02003f9f0fde924ea"
+  instance_type = "t2.micro"
+}
